bf7a08412ee2f88974b094748349c1e35ef39e20
jhan daza <jhand@keoworld.com>
2023-02-12 17:34:45 -0500

    add build

---
+++ build/precache-manifest.2d4ee9927138a64ad713bef193a862e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30f09a8276378a08daa8",
+    "url": "/static/css/main.5eedc8e5.chunk.css"
+  },
+  {
+    "revision": "30f09a8276378a08daa8",
+    "url": "/static/js/main.30f09a82.chunk.js"
+  },
+  {
+    "revision": "aac74624043b7261b7ef",
+    "url": "/static/js/1.aac74624.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ba2addcccf929eae1ee36528eb2204c",
+    "url": "/static/media/TRANSAUTOS PG.8ba2addc.svg"
+  },
+  {
+    "revision": "d14f1a7503498d5eed73c73a3984742f",
+    "url": "/index.html"
+  }
+];
